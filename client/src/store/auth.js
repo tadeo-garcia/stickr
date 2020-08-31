94c8f31d4bdbaa63f73e3eb95e5ef02d8d4f7faa
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-08-31 17:51:11 -0500

    add redux login actions

---
+++ client/src/store/auth.js
@@ -0,0 +1,28 @@
+import Cookies from 'js-cookie';
+
+const SET_USER = 'auth/SET_USER';
+
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    user
+  }
+}
+
+export const login = (username, password) => {
+  return async dispatch => {
+    const res = fetch('/api/session', {
+      method: 'put',
+      headers: {
+        "Content-Type": "application/json",
+        "XSRF-TOKEN": Cookies.get("XSRF-TOKEN")
+      },
+      body: JSON.stringify({ username, password })
+    })
+    res.data = await res.json(); // {user with the scope of currentUser}
+    if (res.ok) {
+      dispatch(setUser(res.data))
+    }
+    return res;
+  }
+}
