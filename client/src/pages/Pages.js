62130b24e5fdbff0d949b4992989bb760d92580c
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-09-01 17:15:41 -0500

    created signup user component, routes, actions, reducers and tested(it works)

--- client/src/pages/Pages.js
+++ client/src/pages/Pages.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import LoginPage from './LoginPage.js';
+import SignupPage from './SignupPage'
 
 export default function Pages() {
   return (
     <>
       <Route path='/login' component={LoginPage} />
+      <Route path='/signup' component={SignupPage} />
     </>
   )
 }
---
+++ client/src/pages/SignupPage.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { signup, login } from '../store/auth';
+import { Redirect } from 'react-router-dom';
+import './signuppage.css';
+
+import LoginNavbar from '../components/LoginNavBar';
+
+
+function SignupPage() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [passwordConfirm, setPasswordConfirm] = useState();
+  const dispatch = useDispatch();
+  const currentUserId = useSelector(state => state.auth.id);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(signup(username, password, passwordConfirm))
+  }
+
+  const handleSubmitDemo = e => {
+    e.preventDefault();
+    dispatch(login('mkue', 'password'))
+  }
+
+  if (currentUserId) return <Redirect to='/' />;
+
+
+  return (
+    <>
+      <LoginNavbar />
+      <div class='main-container' >
+        <div class='form-container'>
+          <div class='form-div'>
+            <div id='logo-div' />
+            <span id='login-text'>Sign up for Stickr</span>
+            <form id='login-form' onSubmit={handleSubmit}>
+              <input class="form-input"
+                type='text'
+                name='username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder='Username' />
+              <input class="form-input"
+                type='password'
+                name='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Password' />
+              <input class="form-input"
+                type='password'
+                name='passwordConfirm'
+                value={passwordConfirm}
+                onChange={(e) => setPasswordConfirm(e.target.value)}
+                placeholder='Confirm Password' />
+              <button id='login-button' type='submit'>Sign up</button>
+            </form>
+            <span onClick={handleSubmitDemo} id='demo'>Login with a demo user?</span>
+            <span id='member-text'>Already a Stickr member?
+              <a href='/login' id='demo'> Log in here.</a>
+            </span>
+          </div>
+        </div>
+      </div >
+    </>
+  )
+};
+
+export default SignupPage;
