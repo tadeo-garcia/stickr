9bb62af32ef57f8ee4e88ad34ea715bd3a78476b
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-10-27 16:45:14 -0500

    removed code for warnings

--- client/src/components/DeletePhotoComponent.js
+++ client/src/components/DeletePhotoComponent.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
-import { deletePhotoById } from '../store/photos';
-import './deletephoto.css';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { deletePhotoById } from "../store/photos";
+import "./deletephoto.css";
 
 function DeletePhotoButton({ currentUserId, photoId }) {
   const dispatch = useDispatch();
   const history = useHistory();
 
-
-  const handleDeletePhoto = e => {
+  const handleDeletePhoto = (e) => {
     e.preventDefault();
-    dispatch(deletePhotoById(photoId))
-    history.push('/dashboard')
-  }
-
+    dispatch(deletePhotoById(photoId));
+    history.push("/dashboard");
+  };
 
   return (
     <div>
-      <span id='delete-button' onClick={handleDeletePhoto}>Delete this picture.</span>
+      <span id="delete-button" onClick={handleDeletePhoto}>
+        Delete this picture.
+      </span>
     </div>
-  )
+  );
 }
 
 export default DeletePhotoButton;
--- client/src/pages/AboutMePage.js
+++ client/src/pages/AboutMePage.js
@@ -1,31 +1,42 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import ModalNavbar from '../components/ModalNavBar';
-import FooterBar from '../components/FooterBar';
-import SingleScrollComponent from '../components/SingleScrollComponent';
-import DashboardSidebar from '../components/DashboardSidebar'
-import './aboutmepage.css';
-
-
+import React from "react";
+import ModalNavbar from "../components/ModalNavBar";
+import FooterBar from "../components/FooterBar";
+import SingleScrollComponent from "../components/SingleScrollComponent";
+import "./aboutmepage.css";
 
 function AboutMePage() {
-
   return (
     <>
       <ModalNavbar />
-      <div className='body-container'>
-        <div className='about-container' >
-          <SingleScrollComponent url={'/pics/background/sticker.png'} />
-          <div className='aboutme-div'>
-            <br /><br /><br /><br /><br /><br /><br />
+      <div className="body-container">
+        <div className="about-container">
+          <SingleScrollComponent url={"/pics/background/sticker.png"} />
+          <div className="aboutme-div">
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
             <h2> Stickr was created using react and redux hooks, as well as express.</h2>
             <br />
-            <h2> To dive deeper into the code, checkout the link to the github repository at the bottom!</h2>
+            <h2>
+              {" "}
+              To dive deeper into the code, checkout the link to the github repository at the
+              bottom!
+            </h2>
             <br />
-            <h2> In addition to the site, all of the stickers that are under the artist name tsoyok, were also created by me!</h2>
+            <h2>
+              {" "}
+              In addition to the site, all of the stickers that are under the artist name tsoyok,
+              were also created by me!
+            </h2>
             <br />
-            <h2>If you have any questions feel free to contact me, through <a href="mailto:tadeoalyta@gmail.com">email</a>.</h2>
+            <h2>
+              If you have any questions feel free to contact me, through{" "}
+              <a href="mailto:tadeoalyta@gmail.com">email</a>.
+            </h2>
             <br />
             <h2>-T. Garcia</h2>
           </div>
@@ -33,8 +44,7 @@ function AboutMePage() {
       </div>
       <FooterBar />
     </>
-  )
-
-};
+  );
+}
 
 export default AboutMePage;
