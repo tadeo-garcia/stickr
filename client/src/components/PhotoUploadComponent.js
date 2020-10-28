56b4c8db60e8307496e70523180b50e51966a1b0
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-10-28 02:32:20 -0500

    FINALLY UPLOADED A PICTURE

--- client/src/components/PhotoUploadComponent.js
+++ client/src/components/PhotoUploadComponent.js
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
-// import { uploadPhoto } from "../store/photos";
+import { useSelector, useDispatch } from "react-redux";
+import { uploadSinglePhoto } from "../store/photos";
 import { useHistory } from "react-router-dom";
 
-import axios from "axios";
 import "./photouploadcomponent.css";
 
 export default function PhotoUploadComponent() {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const history = useHistory();
   const currentUserId = useSelector((state) => state.auth.id);
-  const [file, setFile] = useState("");
-  const [filename, setFilename] = useState("Choose File");
+  const [file, setFile] = useState(null);
   const [fileDescription, setFileDescription] = useState("");
-  const [uploadedFile, setUploadedFile] = useState({});
 
   const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-    setFilename(e.target.files[0].name);
+    setFile({
+      raw: e.target.files[0],
+    });
   };
 
   const handleDescriptionChange = (e) => {
@@ -26,30 +24,10 @@ export default function PhotoUploadComponent() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("description", fileDescription);
-    formData.append("userId", currentUserId);
+    console.log(file);
+    // console.log(fileDescription);
 
-    console.log(formData.get("description"));
-    console.log(formData.get("userId"));
-    console.log(formData.get("file"));
-    // try {
-    //   const res = await axios.post('/api/photos/upload/', formData, {
-    //     headers: {
-    //       'Content-Type': 'multipart/form-data'
-    //     }
-    //   });
-    //   const { fileName, filePath } = res.data;
-    //   setUploadedFile({ fileName, filePath })
-    //   history.push('/dashboard')
-    // } catch (err) {
-    //   if (err.response.status === 500) {
-    //     console.log('There was a problem with the server')
-    //   } else {
-    //     console.log(err.response.data.message)
-    //   }
-    // }
+    dispatch(uploadSinglePhoto(file, currentUserId, fileDescription));
   };
 
   return (
@@ -60,7 +38,7 @@ export default function PhotoUploadComponent() {
             type="file"
             className="upload-photo"
             id="customPhoto"
-            name="resume"
+            name="file"
             onChange={handleFileChange}
           />
         </div>
--- routes/util/awsS3.js
+++ routes/util/awsS3.js
@@ -1,118 +1,67 @@
-const AWS = require("aws-sdk");
-// name of your bucket here
-const NAME_OF_BUCKET = "stickr-app";
-const multer = require('multer');
-const multerS3 = require('multer-s3')
-const secret_key = process.env.AWS_SECRET_ACCESS_KEY
-const access_key = process.env.AWS_ACCESS_KEY_ID
-// else {
-//  make sure to set environment variables in production for:
-//  AWS_ACCESS_KEY_ID
-//  AWS_SECRET_ACCESS_KEY
-//  and aws will automatically use those environment variables
-// }
-const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+// const AWS = require("aws-sdk");
+// const NAME_OF_BUCKET = "stickr-app";
+// const multer = require("multer");
+// // const multerS3 = require("multer-s3");
+// // const secret_key = process.env.AWS_SECRET_ACCESS_KEY;
+// // const access_key = process.env.AWS_ACCESS_KEY_ID;
+// // else {
+// //  make sure to set environment variables in production for:
+// //  AWS_ACCESS_KEY_ID
+// //  AWS_SECRET_ACCESS_KEY
+// //  and aws will automatically use those environment variables
+// // }
+// const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
-const singlePublicFileUpload = async (file) => {
-  const { originalname, mimetype, buffer } = await file;
-  const path = require("path");
-  // name of the file in your S3 bucket will be the date in ms plus the extension name
-  const Key = new Date().getTime().toString() + path.extname(originalname);
-  const uploadParams = {
-    Bucket: NAME_OF_BUCKET,
-    Key,
-    Body: buffer,
-    ACL: "public-read"
-  };
-  const result = await s3.upload(uploadParams).promise();
+// const singlePublicFileUpload = async (file) => {
+//   const { name, mimetype, buffer } = await file;
+//   const path = require("path");
+//   // name of the file in your S3 bucket will be the date in ms plus the extension name
+//   const Key = new Date().getTime().toString() + path.extname(name);
+//   const uploadParams = {
+//     Bucket: NAME_OF_BUCKET,
+//     Key,
+//     Body: buffer,
+//     ACL: "public-read",
+//   };
+//   const result = await s3.upload(uploadParams).promise();
 
-  // save the name of the file in your bucket as the key in your database to retrieve for later
-  return result.Location;
-};
-
-const multiplePublicFileUpload = async files => {
-  return await Promise.all(
-    files.map(file => {
-      return singlePublicFileUpload(file);
-    })
-  );
-};
-
-const singlePrivateFileUpload = async (file) => {
-  const { originalname, mimetype, buffer } = await file;
-  const path = require("path");
-  // name of the file in your S3 bucket will be the date in ms plus the extension name
-  const Key = new Date().getTime().toString() + path.extname(originalname);
-  const uploadParams = {
-    Bucket: NAME_OF_BUCKET,
-    Key,
-    Body: buffer
-  };
-  const result = await s3.upload(uploadParams).promise();
-
-  // save the name of the file in your bucket as the key in your database to retrieve for later
-  return result.Key;
-};
-
-const multiplePrivateFileUpload = async (files) => {
-  return await Promise.all(files.map(file => {
-    return singlePrivateFileUpload(file);
-  }));
-};
-
-const retrievePrivateFile = (key) => {
-  let fileUrl;
-  if (key) {
-    fileUrl = s3.getSignedUrl("getObject", {
-      Bucket: NAME_OF_BUCKET,
-      Key: key
-    });
-  }
-  return fileUrl || key;
-};
-
-const storage = multer.memoryStorage({
-  destination: function (req, file, callback) {
-    callback(null, '');
-  }
-});
-
-
-//bottom two are middlewares for submitting:
-
-const singleMulterUpload = (nameOfKey) => multer({ storage: storage }).single(nameOfKey);
-const multipleMulterUpload = (nameOfKey) => multer({ storage: storage }).array(nameOfKey);
-
-
-AWS.config.update({
-    accessKeyId: access_key,
-    secretAccessKey: secret_key,
-    region: 'us-west-1',
-});
-
-
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: NAME_OF_BUCKET,
-        acl: 'public-read',
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-        key: function (req, file, cb) {
-            cb(null, file.originalname);
-        }
-    })
-})
-
-
-module.exports = {
-  upload,
-  s3
-}
-  // singlePublicFileUpload,
-  // multiplePublicFileUpload,
-  // singlePrivateFileUpload,
-  // multiplePrivateFileUpload,
-  // retrievePrivateFile,
-  // singleMulterUpload,
-  // multipleMulterUpload
+//   // save the name of the file in your bucket as the key in your database to retrieve for later
+//   return result.Location;
 // };
+
+// const storage = multer.memoryStorage({
+//   destination: function (req, file, callback) {
+//     callback(null, "");
+//   },
+// });
+
+// //bottom two are middlewares for submitting:
+
+// const singleMulterUpload = (nameOfKey) => multer({ storage: storage }).single(nameOfKey);
+
+// // AWS.config.update({
+// //   accessKeyId: access_key,
+// //   secretAccessKey: secret_key,
+// //   region: "us-west-1",
+// // });
+
+// // const upload = multer({
+// //   storage: multerS3({
+// //     s3: s3,
+// //     bucket: NAME_OF_BUCKET,
+// //     acl: "public-read",
+// //     contentType: multerS3.AUTO_CONTENT_TYPE,
+// //     key: function (req, file, cb) {
+// //       cb(null, file.originalname);
+// //     },
+// //   }),
+// // });
+
+// // module.exports = {
+// //   singlePublicFileUpload,
+// //   singleMulterUpload,
+// //   s3,
+// // };
+
+// exports.singlePublicFileUpload = singlePublicFileUpload;
+// exports.singleMulterUpload = singleMulterUpload;
